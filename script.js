3b0de7ee61681dabbf710c2839921f54b810d1c7
Pruthvi6797 <104039237+Pruthvi6797@users.noreply.github.com>
2023-03-07 14:51:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+
+// Define an array of colors
+const colors = ['red', 'blue', 'green', 'yellow'];
+
+// Initialize variables
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let started = false;
+
+// Function to play sound based on color name
+function playSound(name) {
+  const sound = new Audio(`sounds/${name}.mp3`);
+  sound.play();
+}
+
+// Function to animate button based on color name
+function animateButton(name) {
+  $(`#${name}`).addClass('pressed');
+  setTimeout(() => {
+    $(`#${name}`).removeClass('pressed');
+  }, 100);
+}
+
+// Function to start a new game
+function nextSequence() {
+  userClickedPattern = [];
+  const randomNumber = Math.floor(Math.random() * 4);
+  const randomChosenColor = colors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+  level++;
+  $('h1').text(`Level ${level}`);
+}
+
+// Function to handle user click on button
+function handleClick() {
+  const userChosenColor = this.id;
+  userClickedPattern.push(userChosenColor);
+  animateButton(userChosenColor);
+  playSound(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+}
+
+// Function to check if user's sequence matches the game's sequence
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (gamePattern.length === userClickedPattern.length) {
+      setTimeout(nextSequence, 1000);
+    }
+  } else {
+    playSound('wrong');
+    $('body').addClass('game-over');
+    setTimeout(() => {
+      $('body').removeClass('game-over');
+    }, 200);
+    $('h1').text('Game Over, Press Any Key to Restart');
+    startOver();
+  }
+}
+
+// Function to start the game
+function startGame() {
+  if (!started) {
+    $('h1').text(`Level ${level}`);
+    nextSequence();
+    started = true;
+  }
+}
+
+// Function to reset the game
+function startOver() {
+  gamePattern = [];
+  userClickedPattern = [];
+  level = 0;
+  started = false;
+}
+
+// Add event listener to button
+$('.btn').click(handleClick);
+
+// Add event listener to document to start game
+$(document).keydown(startGame);
